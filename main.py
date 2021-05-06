24e25d373a79fb05e2b636d7d2689ac6b5188f1e
Ruslan Gazizov <ruslangazizov36@gmail.com>
2021-05-06 23:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,26 +11,24 @@ def input_adjacency_matrix():
         greeting = 'Введите через пробел - номер первой вершины, номер второй вершины, вес ребра: '
         vert1, vert2, weight = map(int, input(greeting).split())
         adjacency_matrix[vert1 - 1][vert2 - 1] = weight
-    # adjacency_matrix = [[0, 3, inf, 5],
-    #                     [2, 0, inf, 4],
-    #                     [inf, 1, 0, inf],
-    #                     [inf, inf, 2, 0]]
+    # adjacency_matrix = [[ 0,   3,  inf,  5 ],
+    #                     [ 2,   0,  inf,  4 ],
+    #                     [inf,  1,   0,  inf],
+    #                     [inf, inf,  2,   0 ]]
     return adjacency_matrix
 
 
 def floyd_warshall(adjacency_matrix):
     distance = [line.copy() for line in adjacency_matrix]
-    pred = {}
-    for u in range(len(adjacency_matrix)):
-        pred[u] = {}
-        for v in range(len(adjacency_matrix)):
-            pred[u][v] = -1
-        for neighbor in range(len(adjacency_matrix[u])):
+    verts = list(range(len(adjacency_matrix)))
+    pred = [[-1 for _ in verts] for _ in verts]
+    for u in verts:
+        for neighbor in verts:
             if 0 != adjacency_matrix[u][neighbor] != inf:
-                pred[u][neighbor] = u  # есть ребро u -> neighbor
-    for k in range(len(adjacency_matrix)):
-        for i in range(len(adjacency_matrix)):
-            for j in range(len(adjacency_matrix)):
+                pred[u][neighbor] = u  # ребро u -> neighbor существует
+    for k in verts:
+        for i in verts:
+            for j in verts:
                 new_dist = distance[i][k] + distance[k][j]
                 if new_dist < distance[i][j]:
                     distance[i][j] = new_dist
@@ -56,9 +54,6 @@ def print_solution(distance, pred):
             if vert1 != vert2:
                 print(f'{vert1 + 1} -> {vert2 + 1} (вес {distance[vert1][vert2]}):',
                       path(vert1, vert2, pred[vert1]))
-    # print('Веса кратчайших путей:')
-    # for line in distance:
-    #     print(*line)
 
 
 def main():
